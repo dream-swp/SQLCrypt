d10bcfcebf128b4286b1c03bc59693f650b24086
Dream <dream-swp@qq.com>
2025-02-18 02:49:18 +0800

    Update v1.2.0

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Dream on 2025/2/18.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
